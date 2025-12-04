16626d8218044a1ad20e87d96c00eecd4c683bcd
Leticia Godoi <86751207+leticiagdi@users.noreply.github.com>
2025-12-04 00:20:17 -0300

    Update api.ts

--- src/front/src/api.ts
+++ src/front/src/api.ts
@@ -1,5 +1,5 @@
 // Define qual URL usar automaticamente
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+export const API_URL = 'https://project-webar.onrender.com';
 
 // (Opcional) Uma funçãozinha para facilitar ainda mais
 export const getApiUrl = (path: string) => {
